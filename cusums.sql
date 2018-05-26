3cc0820c9e565744f040e6c6d4c1bdbd54233795
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-05-26 11:28:50 +0900

    예약 / 메뉴

---
+++ cusums.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.7.22, for Win32 (AMD64)
+--
+-- Host: localhost    Database: midas
+-- ------------------------------------------------------
+-- Server version	5.7.22-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cusums`
+--
+
+DROP TABLE IF EXISTS `cusums`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cusums` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `menu_id` int(11) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `state` int(11) DEFAULT NULL,
+  PRIMARY KEY (`index`),
+  KEY `cc_idx` (`user_id`),
+  KEY `dd_idx` (`menu_id`),
+  CONSTRAINT `cc` FOREIGN KEY (`user_id`) REFERENCES `users` (`index`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `dd` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`index`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cusums`
+--
+
+LOCK TABLES `cusums` WRITE;
+/*!40000 ALTER TABLE `cusums` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cusums` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-26 11:28:17
